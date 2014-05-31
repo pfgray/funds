f3cc3c5e596aaa7df982f6dcbb89ec758836ce0a
Paul Gray <pfbgray@gmail.com>
2014-05-30 20:32:04 -0400

    Adding Font awesome

--- public/script/funds.js
+++ public/script/funds.js
@@ -1,27 +1,21 @@
 $(document).ready(function(){
 
 	$(".signSpan").click(function(){
-		/*
-		var img = $(".signSpan").find("img");
-
-		if(img.attr("alt") == '-'){
-			img.attr("src", "/images/plus.svg");
-			img.attr("alt", "+");
-		}else{
-			img.attr("src", "/images/minus.svg");
-			img.attr("alt", "-");
-		}
-		*/
 		var button = $("#sign");
 		if(button.hasClass('alert')){
 			button.removeClass('alert');
 			button.addClass('success');
-			button.html('+');
+			//switch icons:
+			button.find('i').removeClass('fa-minus');
+			button.find('i').addClass('fa-plus');
+
 			$("#sign_check").prop('checked', false);
 		}else{
 			button.removeClass('success');
 			button.addClass('alert');
-			button.html('-');
+			//switch icons:
+			button.find('i').removeClass('fa-plus');
+			button.find('i').addClass('fa-minus');
 			$("#sign_check").prop('checked', true);
 		}
 	});
@@ -30,16 +24,5 @@ $(document).ready(function(){
 		$("#filter").html("filter: <div class='tag'>"+$(tag.currentTarget).html()+"</div>");
     });
 
-/*
-	$('body').live('swipeleft swiperight',function(event){
-        if (event.type == "swiperight") {
-           alert("swipped right side");       
-        }
-        if (event.type == "swipeleft") {
-            alert("swipped left side");
-        }
-        event.preventDefault();
-    });
-*/
 
 });
