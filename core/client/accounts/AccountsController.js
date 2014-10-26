74a07a10a534d88df55666e6b36402f3b89b05b3
Paul Gray <pfbgray@gmail.com>
2014-10-26 01:15:41 -0400

    Adding account creation

--- core/client/accounts/AccountsController.js
+++ core/client/accounts/AccountsController.js
@@ -1,12 +1,28 @@
 funds.controller('AccountsController', ['$scope', '$http', function($scope, $http){
 
-    $http.get('/api/accounts')
-    .success(function(data){
-        $scope.accounts = data;
-    })
-    .error(function(data){
-        console.error('error:', data);
-        $scope.accounts = [];
-    });
+    var refreshAccounts = function(){
+        $http.get('/api/accounts')
+        .success(function(data){
+            $scope.accounts = data;
+        })
+        .error(function(data){
+            console.error('error:', data);
+            $scope.accounts = [];
+        });
+    }
+    refreshAccounts();
+
+    $scope.createAccount = function(name, type){
+        $http.post('/api/accounts', {
+            name:name,
+            type:type
+        })
+        .success(function(data){
+            refreshAccounts();
+        })
+        .error(function(data){
+            console.error(data);
+        });
+    }
 
 }]);
